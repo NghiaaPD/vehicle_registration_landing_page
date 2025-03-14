177c7b3390860d9e90f62349fbc7728e656386a9
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2025-03-15 00:35:42 +0700

    build:fix astro.config to github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://nghiaapd.github.io/vehicle_registration_landing_page/", // ✅ Đúng URL
+  site: "https://nghiaapd.github.io/", // ✅ Đúng URL
   base: "/vehicle_registration_landing_page/", // ✅ Đúng base path
   output: "static", // 🚀 Đảm bảo build thành HTML tĩnh
 });
