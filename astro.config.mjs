6725d57e368d06366a0eff17f7cc58a631c16d6c
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2025-03-15 00:12:09 +0700

    build: fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://nghiaapd.github.io",
-  base: "/vehicle_registration_landing_page",
-  output: "static",
+  site: "https://nghiaapd.github.io/vehicle_registration_landing_page/", // ✅ Đúng URL
+  base: "/vehicle_registration_landing_page/", // ✅ Đúng base path
+  output: "static", // 🚀 Đảm bảo build thành HTML tĩnh
 });
